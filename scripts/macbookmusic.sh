b0e5b6d7f90d529c916ab74cfecead5d1dabe228
Michael Nye <thenyeguy@gmail.com>
2015-03-05 23:28:17 -0500

    Cleanup of tmux music script

--- scripts/macbookmusic.sh
+++ scripts/macbookmusic.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+
+# Queries a Mac media player using applescript
+function checkMacPlayer () {
+    # First query if the app is running
+    service=$1
+    running=$(osascript -e "tell application \"System Events\" to \
+        (name of processes) contains \"$service\"")
+    if [[ $running == "false" ]]; then return 0; fi
+
+    # If the app is running, then check whether it is playing
+    status=$(osascript -e "tell application \"$service\" to \
+        player state as string")
+    if [[ "$status" == "playing" ]]; then
+        artist=$(osascript -e "tell application \"$service\" to \
+            artist of current track as string")
+        track=$(osascript -e "tell application \"$service\" to \
+            name of current track as string")
+        displaySong $status "$artist" "$track"
+    fi
+}
+
+# Write a tmux prompt segment
 function displaySong () {
     if [ $1 == "playing" ]; then
         color="colour81"
@@ -7,30 +29,13 @@ function displaySong () {
     fi
     icon=""
     echo "#[fg=colour31] #[fg=$color]$icon $2 - $3"
+    exit 0
 }
 
+# If we have applescript, then we are on a Mac
 if type osascript > /dev/null; then
-    # First check iTunes
-    itunes=$(osascript -e 'tell application "iTunes" to player state as string')
-    if [[ "$itunes" == "playing" ]]; then
-        artist=`osascript -e 'tell application "iTunes" to \
-            artist of current track as string'`;
-        track=`osascript -e 'tell application "iTunes" to \
-            name of current track as string'`;
-        displaySong $itunes "$artist" "$track"
-        exit 0
-    fi
-
-    # Then check Spotify
-    spotify=$(osascript -e 'tell application "Spotify" to player state as string')
-    if [[ "$spotify" == "playing" ]]; then
-        artist=`osascript -e 'tell application "Spotify" to \
-            artist of current track as string'`;
-        track=`osascript -e 'tell application "Spotify" to \
-            name of current track as string'`;
-        displaySong $spotify "$artist" "$track"
-        exit 0
-    fi
+    checkMacPlayer iTunes
+    checkMacPlayer Spotify
     exit 2
 else
     exit 1
