0af1f03ba0657e29284ac98529fe6369af230649
Michael Nye <thenyeguy@gmail.com>
2017-03-10 19:54:49 -0800

    Clean up color function

--- fish/functions/colors.fish
+++ fish/functions/colors.fish
@@ -1,11 +1,10 @@
-function show_color
-    set color $argv[1]
-    echo -n (set_color $color)"$color "
-    echo -n (set_color --bold $color)"$color "
-    echo (set_color normal)
-end
-
 function colors
+    function show_color -a color
+        echo -n (set_color $color)"$color "
+        echo -n (set_color --bold $color)"$color "
+        echo (set_color normal)
+    end
+
     echo "normal     "(set_color --bold)"bold"(set_color normal)
     for color in (set_color --print-colors)
         test $color != normal; and show_color $color
