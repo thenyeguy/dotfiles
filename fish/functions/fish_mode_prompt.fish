a3311d3c3496a3b05a80505f495b0c4ea6a66de6
Michael Nye <thenyeguy@gmail.com>
2017-04-07 09:36:24 -0700

    Style fish mode prompt more like vim-airline

--- fish/functions/fish_mode_prompt.fish
+++ fish/functions/fish_mode_prompt.fish
@@ -2,22 +2,22 @@ function fish_mode_prompt --description "Displays the current vi mode"
     set segment_seperator 
 
     function label -S -a color label
-        set_color --bold --background $color black
+        set_color --background $color black
         echo -n " $label "
         set_color --background normal $color
-        echo "$segment_seperator"
+        echo "$segment_seperator "
     end
 
     if test "$fish_key_bindings" = "fish_vi_key_bindings"
         switch $fish_bind_mode
             case default
-                label yellow 'N'
+                label green 'NORMAL'
             case insert
                 echo '         '
             case replace-one
-                label red 'R'
+                label red 'REPLACE'
             case visual
-                label blue V
+                label yellow 'VISUAL'
         end
     end
 end
