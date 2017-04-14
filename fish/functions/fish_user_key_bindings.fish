ca05db9064f8276183a7b21fac17f795eec7b8d7
Michael Nye <thenyeguy@gmail.com>
2017-04-14 10:17:46 -0700

    Fix fzf in fish

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,9 @@
 function fish_user_key_bindings
     fish_vi_key_bindings
-    fzf_key_bindings
+
+    if test -e ~/.fzf/bin
+        set -x PATH ~/.fzf/bin $PATH
+        source ~/.fzf/shell/key-bindings.fish
+        fzf_key_bindings
+    end
 end
