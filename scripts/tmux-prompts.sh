55a4de1b0ac11c9410cd0aacfeaa4102e77ff7c4
Michael Nye <thenyeguy@gmail.com>
2018-03-08 18:33:57 -0800

    Remove current song support.

--- scripts/tmux-prompts.sh
+++ scripts/tmux-prompts.sh
@@ -3,10 +3,6 @@
 # Minimum tmux width to display prompts
 MINWIDTH=150
 
-# Music segment lengths
-MAXARTISTLENGTH=25
-MAXMUSICLENGTH=50
-
 # Battery lengths
 BATTERYLENGTH=15
 
@@ -25,36 +21,6 @@ fi
 
 # Get script directory
 dir=$(dirname $0)
-
-
-# Get current song
-music=$(nc -U $HOME/.currentsong)
-if [ -n "$music" ]; then
-    IFS=$'\t' read title artist state <<< "$music"
-
-    if [ -z "$state" ]; then
-      state="$artist"
-      artist=""
-    fi
-
-    if [ -n "$artist" ]; then
-        if (( ${#artist} > $MAXARTISTLENGTH )); then
-            artist="${artist:0:$MAXARTISTLENGTH-3}..."
-        fi
-        text="$artist - $title"
-    else
-        text="$title"
-    fi
-    if (( ${#text} > $MAXMUSICLENGTH )); then
-        text="${text:0:$MAXMUSICLENGTH-3}..."
-    fi
-    if [ "$state" == "paused" ]; then
-        color="$DIM"
-    else
-        color="$BRIGHT"
-    fi
-    echo -en " #[fg=$DIM]$SEP #[fg=$color]♫ $text"
-fi
 
 
 # Get battery output
