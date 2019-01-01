8210281660597202ec6a2219e5e1d8698fd16b9f
Michael Nye <thenyeguy@gmail.com>
2018-12-31 20:55:19 -0800

    Update prompt to be more git aware

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -5,15 +5,15 @@
 
 # Configure prompt
 set __prompt_segment_seperator 
+set __prompt_subsegment_seperator 
 set __prompt_active_jobs_symbol ☼
 set __prompt_bad_exit_symbol ‼
-set __prompt_git_symbol 
 
-# Configure fish git prompt
-set __fish_git_prompt_describe_style branch
-set __fish_git_prompt_showdirtystate true
-set __fish_git_prompt_char_dirtystate ○
-set __fish_git_prompt_char_stagedstate ◉
+set __prompt_git_symbol 
+set __prompt_git_dirty ○
+set __prompt_git_staged ◉
+
+set fish_prompt_pwd_dir_length 0
 
 ### Starts a new, empty line.
 # This will clear the entire line of any existing contents.
@@ -39,6 +39,11 @@ function __prompt_segment -S -a background foreground
     end
 end
 
+### Draws a new subsegment of the current prompt segment.
+function __prompt_subsegment
+    printf " $__prompt_subsegment_seperator $argv"
+end
+
 ### Clears all segment state and draws any pending segment endings.
 function __prompt_finish_segments
     __prompt_segment normal normal
@@ -46,17 +51,43 @@ function __prompt_finish_segments
 end
 
 ### Draws a git status segment.
-function __prompt_git_segment
+function __prompt_git
     if not silent git rev-parse --git-dir
         return 1
     end
 
     if silent count (git status --short)
-        __prompt_segment yellow black
+        set color yellow
     else
-        __prompt_segment green black
+        set color green
     end
-    __fish_git_prompt " $__prompt_git_symbol %s"
+
+    # Draw repo name.
+    set repo (basename (git rev-parse --show-toplevel))
+    __prompt_segment $color black $__prompt_git_symbol $repo
+
+    # Draw branch name, identifying detached heads.
+    set branch (git symbolic-ref HEAD --short --quiet; set os $status)
+    if test $os -ne 0
+        set branch \((git describe --contains --all HEAD ^/dev/null; \
+                      or git rev-parse --short HEAD)\)
+    end
+    __prompt_subsegment $branch
+
+    # Attach any status symbols to the current branch.
+    git diff --no-ext-diff --quiet --exit-code 2>/dev/null;
+        or set symbols "$symbols$__prompt_git_dirty"
+    git diff-index --cached --quiet HEAD -- 2>/dev/null;
+        or set symbols "$symbols$__prompt_git_staged"
+    if test -n "$symbols"
+        printf " $symbols"
+    end
+
+    # Draw the path relative to the git root, if we aren't already in the root.
+    set path (git rev-parse --show-prefix | string trim --right -c/)
+    if test -n "$path"
+        __prompt_subsegment $path
+    end
 end
 
 ### Draw the actual prompt.
@@ -64,26 +95,26 @@ function fish_prompt
     set last_status $status
 
     __prompt_new_line
-    __prompt_segment cyan black (date "+%l:%M%p" | string trim)
-    test (jobs -l); and __prompt_segment black cyan $__prompt_active_jobs_symbol
+    test (jobs -l);
+        and __prompt_segment black cyan $__prompt_active_jobs_symbol
     test "$last_status" -ne 0; \
         and __prompt_segment black red $__prompt_bad_exit_symbol
-    __prompt_segment black normal $USER@(hostname -s)
-    __prompt_segment blue black (prompt_pwd)
-    __prompt_git_segment
+    __prompt_segment cyan black (date "+%l:%M%p" | string trim)
+    __prompt_git;
+        or __prompt_segment blue black (prompt_pwd)
     __prompt_finish_segments
 
     __prompt_new_line
     switch $fish_bind_mode
         case default
             set_color brred
-            printf " << "
+            printf " « "
             set_color reset
         case visual
             set_color yellow
-            printf " << "
+            printf " « "
             set_color reset
         case "*"
-            printf " >> "
+            printf " » "
     end
 end
