b0c28a18bb60f7183d59bb9d0d552527b0ac9073
Michael Nye <thenyeguy@gmail.com>
2019-02-07 18:58:46 -0800

    Gruvbox up the fish

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,17 +1,17 @@
 function fish_greeting
-    echo '                  '(set_color F00)'___
-   ___======____='(set_color FF7F00)'-'(set_color FF0)'-'(set_color FF7F00)'-='(set_color F00)')
- /T            \_'(set_color FF0)'--='(set_color FF7F00)'=='(set_color F00)')
- [ \ '(set_color FF7F00)'('(set_color FF0)'0'(set_color FF7F00)')   '(set_color F00)'\~    \_'(set_color FF0)'-='(set_color FF7F00)'='(set_color F00)')
-  \      / )J'(set_color FF7F00)'~~    \\'(set_color FF0)'-='(set_color F00)')
-   \\\\___/  )JJ'(set_color FF7F00)'~'(set_color FF0)'~~   '(set_color F00)'\)
-    \_____/JJJ'(set_color FF7F00)'~~'(set_color FF0)'~~    '(set_color F00)'\\
-    '(set_color FF7F00)'/ '(set_color FF0)'\  '(set_color FF0)', \\'(set_color F00)'J'(set_color FF7F00)'~~~'(set_color FF0)'~~     '(set_color FF7F00)'\\
-   (-'(set_color FF0)'\)'(set_color F00)'\='(set_color FF7F00)'|'(set_color FF0)'\\\\\\'(set_color FF7F00)'~~'(set_color FF0)'~~       '(set_color FF7F00)'L_'(set_color FF0)'_
-   '(set_color FF7F00)'('(set_color F00)'\\'(set_color FF7F00)'\\)  ('(set_color FF0)'\\'(set_color FF7F00)'\\\)'(set_color F00)'_           '(set_color FF0)'\=='(set_color FF7F00)'__
-    '(set_color F00)'\V    '(set_color FF7F00)'\\\\'(set_color F00)'\) =='(set_color FF7F00)'=_____   '(set_color FF0)'\\\\\\\\'(set_color FF7F00)'\\\\
-           '(set_color F00)'\V)     \_) '(set_color FF7F00)'\\\\'(set_color FF0)'\\\\JJ\\'(set_color FF7F00)'J\)
-                       '(set_color F00)'/'(set_color FF7F00)'J'(set_color FF0)'\\'(set_color FF7F00)'J'(set_color F00)'T\\'(set_color FF7F00)'JJJ'(set_color F00)'J)
-                       (J'(set_color FF7F00)'JJ'(set_color F00)'| \UUU)
+    echo '                  '(set_color red)'___
+   ___======____='(set_color yellow)'-'(set_color bryellow)'-'(set_color yellow)'-='(set_color red)')
+ /T            \_'(set_color bryellow)'--='(set_color yellow)'=='(set_color red)')
+ [ \ '(set_color yellow)'('(set_color bryellow)'0'(set_color yellow)')   '(set_color red)'\~    \_'(set_color bryellow)'-='(set_color yellow)'='(set_color red)')
+  \      / )J'(set_color yellow)'~~    \\'(set_color bryellow)'-='(set_color red)')
+   \\\\___/  )JJ'(set_color yellow)'~'(set_color bryellow)'~~   '(set_color red)'\)
+    \_____/JJJ'(set_color yellow)'~~'(set_color bryellow)'~~    '(set_color red)'\\
+    '(set_color yellow)'/ '(set_color bryellow)'\  '(set_color bryellow)', \\'(set_color red)'J'(set_color yellow)'~~~'(set_color bryellow)'~~     '(set_color yellow)'\\
+   (-'(set_color bryellow)'\)'(set_color red)'\='(set_color yellow)'|'(set_color bryellow)'\\\\\\'(set_color yellow)'~~'(set_color bryellow)'~~       '(set_color yellow)'L_'(set_color bryellow)'_
+   '(set_color yellow)'('(set_color red)'\\'(set_color yellow)'\\)  ('(set_color bryellow)'\\'(set_color yellow)'\\\)'(set_color red)'_           '(set_color bryellow)'\=='(set_color yellow)'__
+    '(set_color red)'\V    '(set_color yellow)'\\\\'(set_color red)'\) =='(set_color yellow)'=_____   '(set_color bryellow)'\\\\\\\\'(set_color yellow)'\\\\
+           '(set_color red)'\V)     \_) '(set_color yellow)'\\\\'(set_color bryellow)'\\\\JJ\\'(set_color yellow)'J\)
+                       '(set_color red)'/'(set_color yellow)'J'(set_color bryellow)'\\'(set_color yellow)'J'(set_color red)'T\\'(set_color yellow)'JJJ'(set_color red)'J)
+                       (J'(set_color yellow)'JJ'(set_color red)'| \UUU)
                         (UU)'(set_color normal)
 end
