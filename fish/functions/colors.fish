c5252a02e1b2cff45cbc21b70681c8966c5191dc
Michael Nye <thenyeguy@gmail.com>
2019-04-04 09:50:29 -0700

    Add dim mode to colors script

--- fish/functions/colors.fish
+++ fish/functions/colors.fish
@@ -2,11 +2,18 @@ function colors
     function show_color -a color
         echo -n (set_color $color)"$color "
         echo -n (set_color --bold $color)"$color "
+        echo -n (set_color --dim $color)"$color "
         echo (set_color normal)
     end
 
-    echo "normal     "(set_color --bold)"bold"(set_color normal)
-    for color in (set_color --print-colors)
-        test $color != normal; and show_color $color
+    begin
+        echo -n "normal "
+        echo -n (set_color --bold)"bold "
+        echo -n (set_color --dim)"dim"
+        echo (set_color normal)
+
+        for color in (set_color --print-colors)
+            test $color != normal; and show_color $color
+        end
     end | column -t
 end
