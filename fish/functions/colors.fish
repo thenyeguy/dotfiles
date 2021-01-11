8c3e1315f52eb2bd7fe8fbd9c5a128d129917ae8
Michael Nye <thenyeguy@gmail.com>
2021-01-11 15:55:12 -0800

    Fix fish `colors` command.
    Fixes escaping stuff so that it renders correctly, and add reverse mode
    for seeing background colors. I think this only seems relevant in tmux
    for whatever reason, but it is still better.

--- fish/functions/colors.fish
+++ fish/functions/colors.fish
@@ -1,16 +1,19 @@
 function colors
     function show_color -a color
-        echo -n (set_color $color)"$color "
-        echo -n (set_color --bold $color)"$color "
-        echo -n (set_color --dim $color)"$color "
-        echo (set_color normal)
+        set_color normal
+        set_color $color; echo -n "$color "
+        set_color --bold; echo -n "$color "
+        set_color --dim;  echo -n "$color "
+        set_color normal; set_color --reverse $color; echo -n "$color "
+        set_color normal; echo ""
     end
 
     begin
-        echo -n "normal "
-        echo -n (set_color --bold)"bold "
-        echo -n (set_color --dim)"dim"
-        echo (set_color normal)
+        set_color normal;    echo -n "normal "
+        set_color --bold;    echo -n "bold "
+        set_color --dim;     echo -n "dim "
+        set_color --reverse; echo -n "reverse "
+        set_color normal;    echo ""
 
         for color in (set_color --print-colors)
             test $color != normal; and show_color $color
