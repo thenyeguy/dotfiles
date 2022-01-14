af9e715eb37e74e6530a06062585d73cf96e416a
Michael Nye <thenyeguy@gmail.com>
2022-01-14 11:00:22 -0800

    Override more fish syntax colors.

---
+++ fish/functions/fish_set_colors.fish
@@ -0,0 +1,7 @@
+function fish_set_colors
+    set -U fish_color_command white --bold
+    set -U fish_color_comment brblack --dim
+    set -U fish_color_error brred
+    set -U fish_color_param blue
+    set -U fish_color_quote green
+end
--- fish/functions/set_fish_colors.fish
+++
@@ -1,4 +0,0 @@
-function set_fish_colors
-    set -U fish_color_command white --bold
-    set -U fish_color_param blue
-end
