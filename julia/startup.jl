a0a681d09e40fd802df571ac1957aab2ae238208
Michael Nye <thenyeguy@gmail.com>
2022-02-23 16:12:43 -0800

    Enable inline plots when julia is run from kitty

--- julia/startup.jl
+++ julia/startup.jl
@@ -5,4 +5,12 @@ atreplinit() do repl
     @eval using Plots
     @eval using Revise
     @eval using StatsBase
+
+    # Configure kitty show plots inline.
+    if ENV["TERM"] == "xterm-kitty"
+        @eval using KittyTerminalImages
+        Base.invokelatest(set_kitty_config!, :scale, 2.0)
+        Base.invokelatest(set_kitty_config!, :prefer_png_to_svg, false)
+        Base.invokelatest(Plots.theme, :gruvbox_dark)
+    end
 end
