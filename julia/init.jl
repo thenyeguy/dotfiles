8db561f374922606f2220b2145b05f9d9b378fdd
Michael Nye <thenyeguy@gmail.com>
2022-02-24 18:43:33 -0800

    Add julia init script for downloading packages.

---
+++ julia/init.jl
@@ -0,0 +1,12 @@
+#!/usr/bin/env julia
+
+using Pkg
+
+Pkg.add("Combinatorics")
+Pkg.add("DataStructures")
+Pkg.add("DSP")
+Pkg.add("FFTW")
+Pkg.add("Plots")
+Pkg.add("Revise")
+Pkg.add("StatsBase")
+Pkg.add("WAV")
