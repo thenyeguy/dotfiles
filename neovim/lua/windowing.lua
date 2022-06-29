2116f0ba4f8d9076117c27fd99e26966e2bebea3
Michael Nye <thenyeguy@gmail.com>
2022-06-29 10:42:03 -0700

    Tweak neovim windowing behavior.
    * Adjusts the resize on enter command to be less spastic.
    * Make cursorline disappear in unfocused windows

--- neovim/lua/windowing.lua
+++ neovim/lua/windowing.lua
@@ -1,21 +1,26 @@
 -- Fix an issue where neovim doesn't properly handle resizes during startup.
+-- Caused by a tmux hook that runs immediately after a new pane is created.
 vim.api.nvim_create_autocmd({"VimEnter"}, {
-  callback = function()
-    local pid, WINCH = vim.fn.getpid(), vim.loop.constants.SIGWINCH
-    vim.defer_fn(function() vim.loop.kill(pid, WINCH) end, 20)
-  end
+    callback = function()
+        vim.loop.kill(vim.fn.getpid(), vim.loop.constants.SIGWINCH)
+    end,
+    once = true
 })
 
 -- Automatically resize splits
-vim.api.nvim_create_autocmd("VimResized", {
-    command = "wincmd ="
-})
+vim.api.nvim_create_autocmd("VimResized", { command = "wincmd =" })
+
+-- Hide cursorline when windows lose focus
+vim.api.nvim_create_autocmd("FocusLost", { command = "set nocursorline" })
+vim.api.nvim_create_autocmd("FocusGained", { command = "set cursorline" })
+vim.api.nvim_create_autocmd("WinLeave", { command = "setlocal nocursorline" })
+vim.api.nvim_create_autocmd("WinEnter", { command = "setlocal cursorline" })
 
 -- Intelligently calculate split direction
 vim.keymap.set("", "<leader>s", function()
     if vim.api.nvim_win_get_width(0) > 150 then
-        vim.cmd([[vsplit]])
+        vim.cmd("vsplit")
     else
-        vim.cmd([[split]])
+        vim.cmd("split")
     end
 end)
