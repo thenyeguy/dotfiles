6e75a27bd1c66cc2c207f1b0461064fd953d6e5e
Michael Nye <thenyeguy@gmail.com>
2023-12-17 11:10:34 -0800

    Improve wezterm tab management

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,6 +4,11 @@ local config = {}
 --
 -- Color scheme
 config.color_scheme = "Gruvbox Dark (Gogh)"
+config.colors = {
+    tab_bar = {
+        active_tab = { fg_color="#ddd", bg_color='#444', intensity="Bold" },
+    },
+}
 
 --
 -- Font config
@@ -19,26 +24,37 @@ config.underline_thickness = "1pt"
 
 --
 -- Editor appearance
-config.hide_tab_bar_if_only_one_tab = true
 config.inactive_pane_hsb = { saturation = 1.0, brightness = 1.0 }
-config.window_padding = {
-  left = "0cell",
-  right = "0cell",
-  top = "0.0cell",
-  bottom = "0cell",
-}
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.window_padding = { top="3pt", bottom="3pt", left="0pt", right="0pt" }
+config.use_resize_increments = true
 
 --
 -- Keybindings
 local action = wezterm.action
+local RenameTabInteractive = wezterm.action.PromptInputLine({
+    description = wezterm.format({{Attribute={Intensity="Bold"}}, {Text="Rename tab:"}}),
+    action = wezterm.action_callback(function(window, pane, line)
+      if line then
+        window:active_tab():set_title(line)
+      end
+    end),
+})
+
 config.keys = {
     -- App management
     { key="h", mods="CMD|SHIFT", action=action.HideApplication },
+    { key="r", mods="CMD|SHIFT", action=action.ReloadConfiguration },
+    -- Tab management
+    { key="r", mods="CMD", action=RenameTabInteractive },
+    { key="[", mods="CMD|OPT", action=action.MoveTabRelative(-1) },
+    { key="]", mods="CMD|OPT", action=action.MoveTabRelative(1) },
     -- Split management
     { key="s", mods="CMD", action=action.SplitVertical },
     { key="s", mods="CMD|SHIFT", action=action.SplitHorizontal },
     { key="w", mods="CMD", action=action.CloseCurrentPane { confirm=false } },
     { key="w", mods="CMD|SHIFT", action=action.CloseCurrentTab { confirm=true } },
+    { key="z", mods="CMD", action=action.TogglePaneZoomState },
     -- Pane navigation
     { key="[", mods="CMD", action=action.ActivatePaneDirection "Prev" },
     { key="]", mods="CMD", action=action.ActivatePaneDirection "Next" },
