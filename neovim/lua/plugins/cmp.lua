99ccf930fe8bbcb91ead182a051cc3b996156848
Michael Nye <thenyeguy@gmail.com>
2024-01-16 22:43:27 -0800

    Further nvim-cmp tweaking

--- neovim/lua/plugins/cmp.lua
+++ neovim/lua/plugins/cmp.lua
@@ -4,6 +4,7 @@ return {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-nvim-lsp-signature-help",
         "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-cmdline",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-vsnip",
         "hrsh7th/vim-vsnip",
@@ -32,7 +33,6 @@ return {
                 format = require("lspkind").cmp_format({
                     ellipsis_char = "…",
                     mode = "symbol",
-                    symbol_map = { TypeParameter = "" },
                 }),
             },
             mapping = cmp.mapping.preset.insert({
@@ -48,6 +48,7 @@ return {
                 end),
                 ["<C-p>"] = if_visible(cmp.select_prev_item),
             }),
+            preselect = cmp.PreselectMode.None,
             snippet = {
                 expand = function(args) vim.fn["vsnip#anonymous"](args.body) end,
             },
@@ -55,17 +56,58 @@ return {
                 { name = "nvim_lsp" },
                 { name = "nvim_lsp_signature_help" },
             }, {
-                { name = "buffer" },
+                {
+                    name = "buffer",
+                    option = {
+                        get_bufnrs = function()
+                            -- Pull keywords from all visible buffers
+                            local bufs = {}
+                            for _, win in ipairs(vim.api.nvim_list_wins()) do
+                                bufs[vim.api.nvim_win_get_buf(win)] = true
+                            end
+                            return vim.tbl_keys(bufs)
+                        end,
+                    },
+                },
                 { name = "path" },
             }),
             window = {
-                completion = cmp.config.window.bordered({ col_offset = -3 }),
+                completion = cmp.config.window.bordered({ col_offset = -4 }),
                 documentation = cmp.config.window.bordered(),
             },
         })
 
+        -- Enable completion in command line.
+        cmp.setup.cmdline(":", {
+            formatting = { fields = { "abbr", "menu" } },
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = "cmdline" },
+                { name = "path" },
+            }),
+        })
+
+        -- Enable completion when searching.
+        local search_opts = {
+            completion = { autocomplete = false },
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = { { name = "buffer" } },
+        }
+        cmp.setup.cmdline("/", search_opts)
+        cmp.setup.cmdline("?", search_opts)
+
+        -- Hide completion by default in text files.
         cmp.setup.filetype({ "gitcommit", "markdown", "text" }, {
             completion = { autocomplete = false },
         })
+
+        -- Pad symbol map so icons show up double-wide.
+        require("lspkind").init({
+            symbol_map = { TypeParameter = "" },
+        })
+        local symbols = require("lspkind").symbol_map
+        for key, _ in pairs(symbols) do
+            symbols[key] = symbols[key] .. " "
+        end
     end,
 }
