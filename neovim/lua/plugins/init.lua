72a057aaa330b36f39555f85b2a7e00759a30c1d
Michael Nye <thenyeguy@gmail.com>
2024-06-27 11:13:05 -0700

    Fix bad tint on whitespace

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -21,6 +21,7 @@ return {
                 "EndOfBuffer",
                 "LineNr",
                 "MiniIndentscopeSymbol",
+                "Whitespace",
                 "WinSeparator",
             },
         },
